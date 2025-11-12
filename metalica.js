335781cbe560b89945240e3477c408e00926a0b9
Fonq <xfertownx@gmail.com>
2025-11-12 12:14:12 +0100

    imagenes y video mejor organizados

---
+++ metalica.js
@@ -0,0 +1,25 @@
+function openModal(element) {
+  const modal = document.getElementById('lampModal');
+  const img = element.querySelector('img').src;
+  const name = element.getAttribute('data-name');
+  const description = element.getAttribute('data-description');
+  const price = element.getAttribute('data-price');
+
+  document.querySelector('.modal-img').src = img;
+  document.getElementById('modalName').innerText = name;
+  document.getElementById('modalDescription').innerText = description;
+  document.getElementById('modalPrice').innerText = price;
+
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('lampModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById('lampModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
